f91839b9b415bdea0f7ab16d49243500b7f514a1
crimsonpython24 <warrenyjwang@gmail.com>
2021-05-30 13:40:40 +0800

    completed user model

---
+++ nttracker/accounts/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.text import slugify
+
+
+class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    slug = models.SlugField(unique=True)
+    avatar = models.ImageField(_('avatar'), blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        app_label = 'accounts'
+
+    def __str__(self):
+        return self.username
