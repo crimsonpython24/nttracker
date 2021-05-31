cb5e67e119bbf9c5f7eef7003407b9db0fa00d18
crimsonpython24 <warrenyjwang@gmail.com>
2021-05-31 14:43:39 +0800

    #__12: still fixing loading bar

--- nttracker/frontend/src/auth/login-dom.js
+++ nttracker/frontend/src/auth/login-dom.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 
 import "antd/dist/antd.css";
-import { Form, Input, Button, Checkbox, Tooltip, message } from 'antd';
+import { Form, Input, Button, Checkbox, Tooltip, message, Card } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 import { useHistory } from "react-router-dom";
@@ -104,31 +104,33 @@ function Login() {
     history.push("/accounts/profile");
   } else {
     return (
-      <div style={{ marginLeft: "auto", marginRight: "auto", maxWidth: "380px", padding: "30px" }}>
-        <Form name="normal_login" className="login-form" initialValues={{ remember: true }}
-          onFinish={handleSubmit(onSubmit)} requiredMark={false}>
-          <Form.Item name="Username" {...usernameProps}>
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username"
-              name="username" onChange={handleUsernameChange}/>
-          </Form.Item>
-          <Form.Item name="Password" {...passwordProps}>
-            <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password"
-              name="password" onChange={handlePasswordChange} />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
+      <div>
+        <Card style={{ marginLeft: "auto", marginRight: "auto", maxWidth: "380px", marginTop: 100, padding: "10px" }}>
+          <Form name="normal_login" className="login-form" initialValues={{ remember: true }}
+            onFinish={handleSubmit(onSubmit)} requiredMark={false}>
+            <Form.Item name="Username" {...usernameProps}>
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username"
+                name="username" onChange={handleUsernameChange}/>
             </Form.Item>
-            <Tooltip placement="right" trigger="click" title="DM snaakie on Discord, not putting my email here">
-              <span className="login-form-forgot">Forgot password?</span>
-            </Tooltip>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button" loading={loadings}>
-              Log in
-            </Button>
-          </Form.Item>
-        </Form> 
+            <Form.Item name="Password" {...passwordProps}>
+              <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password"
+                name="password" onChange={handlePasswordChange} />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+              <Tooltip placement="right" trigger="click" title="DM snaakie on Discord, not putting my email here">
+                <span className="login-form-forgot">Forgot password?</span>
+              </Tooltip>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button" loading={loadings}>
+                Log in
+              </Button>
+            </Form.Item>
+          </Form> 
+        </Card>
       </div>
     )
   }
--- nttracker/frontend/src/auth/profile-dom.js
+++ nttracker/frontend/src/auth/profile-dom.js
@@ -1,15 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 
 import "antd/dist/antd.css";
 import './profile-dom.css';
-import { message, Form, Input, Button, Tooltip, Divider, Typography, Space, Modal, Card } from "antd";
-import { WarningOutlined, ExclamationCircleOutlined, EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { message, Form, Input, Button, Tooltip, Divider, Typography, Space, Modal, Card, Row, Col, Progress } from "antd";
+import { WarningOutlined, ExclamationCircleOutlined, EyeInvisibleOutlined, EyeTwoTone, LoadingOutlined } from "@ant-design/icons";
 
 import { useHistory } from "react-router-dom";
-import { useMediaQuery } from 'react-responsive'
 
 import { NTTrackerContext } from "../nttracker/context.js";
-
 
 const { Text, Link } = Typography;
 const { confirm } = Modal;
@@ -19,35 +18,15 @@ function Profile() {
   const history = useHistory();
   const [form] = Form.useForm();
   const formLayout = 'vertical';
-  const large_screen = useMediaQuery({ query: '(min-width: 1030px)' })
-  const medium_screen = useMediaQuery({ query: '(min-width: 575px)' })
-  let has_auth, formItemLayout;
+  let has_auth;
 
   if (state.user.authenticated)
     localStorage.setItem('ever_logged_in', true);
   has_auth = localStorage.getItem('ever_logged_in');
 
   if (!has_auth) {
-    message.warning('Please log in to edit account', 3.55);
+    message.warning('Please log in to edit account', 5.35);
     history.push("/accounts/login");
-  }
-  
-  formItemLayout = {
-    wrapperCol: {
-      span: 14,
-      offset: 2
-    },
-    labelCol: { offset: 2 },
-  }
-
-  if (large_screen) {
-    formItemLayout.wrapperCol.span = 10;
-    formItemLayout.wrapperCol.offset = 6;
-    formItemLayout.labelCol.offset = 6;
-  } else {
-    formItemLayout.wrapperCol.span = 13;
-    formItemLayout.wrapperCol.offset = 4;
-    formItemLayout.labelCol.offset = 4;
   }
 
   function showPromiseConfirm() {
@@ -57,8 +36,8 @@ function Profile() {
       content: (
         <>
           <Space direction="vertical" size={0}>
-            <Text>This action cannot be undone! All your data will be lost, but the events you've created will remain.</Text>
-            <Text type="secondary">Click "cancel" or press ESC to quit operation</Text>
+            <Text>This action cannot be reverted! All your data will be lost but the events you've created will remain.</Text>
+            <Text type="secondary">Click "cancel" or press ESC to quit operation. Enter your password to proceed.</Text>
             <Input.Password
               placeholder="Type password to continue..."
               style={{ marginTop: 20 }}
@@ -79,65 +58,54 @@ function Profile() {
   
   return (
     <div>
-      <Card style= {{ display: "inline-flex !important", flexDirection: "row" }}>
-        <Form
-          {...formItemLayout}
-          layout={formLayout}
-          form={form}
-          initialValues={{
-            layout: formLayout,
-          }}
-        >
-          <Form.Item label="Username">
-            <Input placeholder={state.user.username} disabled={true} style={{ maxWidth: 530 }}
-            suffix = {
-              <Tooltip placement="rightTop"
-                title={"Changing username is disabled for security reasons.\
-                        Please contact the site owner."}>
-                  <WarningOutlined 
-                  style={{ fontSize: 17, verticalAlign: "middle", marginRight: 15 }}
+      <Row>
+        <Col xs={1} sm={3} md={5} lg={6} xl={7}></Col>
+        <Col xs={22} sm={18} md={14} lg={12} xl={10}>
+          <Card>
+            <Form layout={formLayout} form={form}>
+              <Form.Item label="Username">
+                <Input placeholder={state.user.username} disabled={true} style={{ maxWidth: 560 }}
+                suffix = {
+                  <Tooltip placement="rightTop"
+                    title={"Changing username is disabled for security reasons.\
+                            Please contact the site owner."}>
+                      <WarningOutlined 
+                      style={{ fontSize: 17, verticalAlign: "middle", marginRight: 15 }}
+                    />
+                  </Tooltip>
+                  }
                 />
-              </Tooltip>
-              }
-            />
-          </Form.Item>
-        </Form>
-        <Divider/>
-        <Form
-          {...formItemLayout}
-          layout={formLayout}
-          form={form}
-          initialValues={{
-            layout: formLayout,
-          }}
-        >
-          <Form.Item label="Current Password">
-            <Input style={{ maxWidth: 530 }}/>
-          </Form.Item>
-          <Form.Item label="New Password">
-            <Input style={{ maxWidth: 530 }}/>
-          </Form.Item>
-          <Form.Item label="Confirm Password">
-            <Input style={{ maxWidth: 530 }}/>
-          </Form.Item>
-          <Form.Item >
-            <Button>Change Password</Button>
-          </Form.Item>
-        </Form>
-        <Divider/>
-        <Form
-          {...formItemLayout}
-          layout={formLayout}
-          form={form}
-          initialValues={{
-            layout: formLayout,
-          }}
-        >
-          <Form.Item label={<Space direction="vertical" size={0} style={{ marginBottom: 10 }}><Text>Delete Account</Text><Text type="secondary">Permanently removes your account from the database (cannot be undone!)</Text></Space>}>
-            <Button danger onClick={showPromiseConfirm}>Delete Account</Button>
-          </Form.Item>
-        </Form>
-      </Card>
+              </Form.Item>
+            </Form>
+            <Divider/>
+            <Form layout={formLayout} form={form}>
+              <Form.Item label="Current Password">
+                <Input style={{ maxWidth: 560 }}/>
+              </Form.Item>
+              <Form.Item label="New Password">
+                <Input style={{ maxWidth: 560 }}/>
+              </Form.Item>
+              <Form.Item label="Confirm Password">
+                <Input style={{ maxWidth: 560 }}/>
+              </Form.Item>
+              <Form.Item >
+                <Button>Change Password</Button>
+              </Form.Item>
+            </Form>
+            <Divider/>
+            <Form layout={formLayout} form={form}>
+              <Form.Item label={
+                <Space direction="vertical" size={0} style={{ marginBottom: 10 }}>
+                  <Text>Delete Account</Text>
+                  <Text type="secondary">Permanently removes your account from the database (cannot be undone!)</Text>
+                  </Space>
+                }>
+                <Button danger onClick={showPromiseConfirm}>Delete Account</Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
