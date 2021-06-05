d10c991d20fd4f04477b2ee5837bfc46170c921a
crimsonpython24 <warrenyjwang@gmail.com>
2021-06-05 12:59:21 +0800

    #__35: configured huey periodic task*

---
+++ nttracker/data/tasks.py
@@ -0,0 +1,13 @@
+from huey import crontab
+from huey.contrib.djhuey import periodic_task, task
+
+
+@task()
+def count_beans(number):
+    print('-- counted %s beans --' % number)
+    return 'Counted %s beans' % number
+
+
+@periodic_task(crontab(minute='*/1'))
+def every_five_mins():
+    print('Every five minutes this will be printed by the consumer')
--- nttracker/nttracker/settings.py
+++ nttracker/nttracker/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'data.apps.DataConfig',
     'rest_framework',
     'corsheaders',
+    'huey.contrib.djhuey',
 ]
 
 MIDDLEWARE = [
@@ -153,3 +154,37 @@ LOGOUT_REDIRECT_URL = '/'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000',] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
 CORS_ORIGIN_REGEX_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000',]
+
+
+# Huey settings
+HUEY = {
+    'huey_class': 'huey.RedisHuey',  # Huey implementation to use.
+    'name': DATABASES['default']['NAME'],  # Use db name for huey.
+    'results': True,  # Store return values of tasks.
+    'store_none': False,  # If a task returns None, do not save to results.
+    'immediate': False,  # If DEBUG=True, run synchronously.
+    'utc': True,  # Use UTC for all times internally.
+    'blocking': True,  # Perform blocking pop rather than poll Redis.
+    'connection': {
+        'host': 'localhost',
+        'port': 6379,
+        'db': 0,
+        'connection_pool': None,  # Definitely you should use pooling!
+        # ... tons of other options, see redis-py for details.
+
+        # huey-specific connection parameters.
+        'read_timeout': 1,  # If not polling (blocking pop), use timeout.
+        'url': None,  # Allow Redis config via a DSN.
+    },
+    'consumer': {
+        'workers': 1,
+        'worker_type': 'thread',
+        'initial_delay': 0.1,  # Smallest polling interval, same as -d.
+        'backoff': 1.15,  # Exponential backoff using this rate, -b.
+        'max_delay': 10.0,  # Max possible polling interval, -m.
+        'scheduler_interval': 1,  # Check schedule every second, -s.
+        'periodic': True,  # Enable crontab feature.
+        'check_worker_health': True,  # Enable worker health checks.
+        'health_check_interval': 1,  # Check worker health every second.
+    },
+}
