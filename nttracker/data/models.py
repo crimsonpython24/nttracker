fe4f8205f4ae41b38a72778efcdd3e0b10c1d82b
unknown <warrenyjwang@gmail.com>
2021-06-11 14:36:52 +0800

    #__39: added json and db structure (finally

--- nttracker/data/models.py
+++ nttracker/data/models.py
@@ -3,25 +3,38 @@ from django.db import models
 from django_unixdatetimefield import UnixDateTimeField
 
 
-class Racer(models.Model):
+# Table 1: Main Data (preserved forever)
+class RaceData(models.Model):
     racer_id = models.IntegerField(primary_key=True)
+    team_id = models.IntegerField()
+    timestamp = UnixDateTimeField()
     races = models.IntegerField()
     time = models.IntegerField()
     typed = models.IntegerField()
     errs = models.IntegerField()
-    joinStamp = UnixDateTimeField()
-    lastActivity = models.IntegerField()
+
+
+# Table 2: User information (one year, updated once per week)
+class RacerLog(models.Model):
+    racer_id = models.IntegerField(primary_key=True)
+    team_id = models.IntegerField()
     role = models.CharField(max_length=10)
     username = models.CharField(max_length=200)
     display_name = models.CharField(max_length=100)
     total_races = models.IntegerField()
     speed = models.IntegerField()
+
+
+# Table 3: User information (no history)
+class RacerData(models.Model):
+    racer_id = models.IntegerField(primary_key=True)
+    joinStamp = UnixDateTimeField()
+    lastActivity = models.IntegerField()
     last_login = UnixDateTimeField()
 
 
-class RaceData(models.Model):
-    timestamp = UnixDateTimeField(primary_key=True)
-    team_id = models.IntegerField()
+# Table 4: Team information (no history)
+class TeamData(models.Model):
+    team_id = models.IntegerField(primary_key=True)
     tag = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
-    members = models.ForeignKey(Racer, on_delete=models.CASCADE)
--- nttracker/data/tasks.py
+++ nttracker/data/tasks.py
@@ -1,5 +1,5 @@
-import requests
-import cloudscraper
+import nitrotype
+import json
 
 from huey import crontab
 from huey.contrib.djhuey import periodic_task, task
@@ -7,6 +7,6 @@ from huey.contrib.djhuey import periodic_task, task
 
 @periodic_task(crontab(minute='*/1'))
 def every_five_mins():
-    scraper = cloudscraper.create_scraper()
-    
-    
+    team = nitrotype.Team('PR2W')
+    with open('random.txt', 'w') as text:
+        text.write(json.dumps(team.data))
