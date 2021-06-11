ea59804c4a3175721e849c91b268cc805eb53d96
unknown <warrenyjwang@gmail.com>
2021-06-11 20:22:04 +0800

    #__40: nevermind, database still not configured yet

--- nttracker/data/tasks.py
+++ nttracker/data/tasks.py
@@ -1,12 +1,34 @@
-import nitrotype
+import nitrotype   # clone adl212's repo lmao
 import json
+import datetime
 
 from huey import crontab
 from huey.contrib.djhuey import periodic_task, task
+
+from .models import RaceData, RacerLog, RacerData, TeamData
 
 
 @periodic_task(crontab(minute='*/1'))
 def every_five_mins():
     team = nitrotype.Team('PR2W')
-    with open('random.txt', 'w') as text:
-        text.write(json.dumps(team.data))
+
+    team_id = team.data["info"]["teamID"]
+    timestamp = datetime.datetime.now()
+    
+    for members in team.data["members"]:
+        racer_id = members["userID"]
+        races = members["played"]
+        time = members["secs"]
+        typed = members["typed"]
+        errs = members["errs"]
+
+        rcd = RaceData(
+            racer_id=racer_id,
+            team_id=team_id,
+            timestamp=timestamp,
+            races=races,
+            time=time,
+            typed=typed,
+            errs=errs
+        )
+        rcd.save()
