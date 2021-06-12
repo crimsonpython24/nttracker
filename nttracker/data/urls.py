1373287e66fcfab11303453462e67e357660d413
unknown <warrenyjwang@gmail.com>
2021-06-12 19:12:53 +0800

    #__43: added json views

--- nttracker/data/urls.py
+++ nttracker/data/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
 
 urlpatterns = [
+    path('racedata_json/', views.racedata_json, name='racedata_json'),
+    path('racerlog_json/', views.racerlog_json, name='racerlog_json'),
+    path('racerdata_json/', views.racerdata_json, name='racerdata_json'),
+    path('teamdata_json/', views.teamdata_json, name='teamdata_json'),
 ]
--- nttracker/data/views.py
+++ nttracker/data/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import RaceData, RacerLog, RacerData, TeamData
+
+
+def racedata_json(request):
+    data = list(RaceData.objects.values())
+    return JsonResponse({'data': data})
+
+
+def racerlog_json(request):
+    data = list(RacerLog.objects.values())
+    return JsonResponse({'data': data})
+
+
+def racerdata_json(request):
+    data = list(RacerData.objects.values())
+    return JsonResponse({'data': data})
+
+
+def teamdata_json(request):
+    data = list(TeamData.objects.values())
+    return JsonResponse({'data': data})
